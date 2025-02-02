caf7be893ee538812f8076b228b5d4b20c8aab66
adity72dd <mmunnabot7@gmail.com>
2025-02-02 15:46:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7613116566:AAGzS53ZbxJBzBe78O-5AqdUEAEEIBaKAPU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Riyahacksyt")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6479495033")
+
+
+
+
+
+
+
+  
